14816aca6581bdab36b4492e0786ff091ec77ffc
asteroidefilmes <vegas@asteroide.tv>
2016-12-20 19:28:34 -0200

    Add files via upload

--- asteroidDev.js
+++ asteroidDev.js
@@ -5,8 +5,7 @@ $(document).ready(function(){
   var options = {
     default_time: 350,
     default_scroll_time: 0,
-    default_distance: 0,
-    default_animation: "fadeleft"
+    default_distance: 0
   };
   MultiScreen.init(options);
   //grid alignment problem in safari
