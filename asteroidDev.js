77c938dfcab7a99ccae7971f2327bf8396d3b0e6
asteroidefilmes <vegas@asteroide.tv>
2018-06-07 10:24:35 -0300

    Update asteroidDev.js

--- asteroidDev.js
+++ asteroidDev.js
@@ -301,7 +301,7 @@ $(document).ready(function(){
      targets.push({name:'ImageDirector1',x:0,y:164,hitArray:[]});
      targets.push({name:'ImageDirector2',x:176,y:153,hitArray:[]});
      targets.push({name:'ImageDirector3',x:363,y:130,hitArray:[]});
-     targets.push({name:'ImageDirector4',x:511,y:77,hitArray:[]});
+     targets.push({name:'ImageDirector4',x:535,y:141,hitArray:[]});
      targets.push({name:'ImageDirector5',x:113,y:14,hitArray:[]});
      targets.push({name:'ImageDirector6',x:299,y:13,hitArray:[]});
      targets.push({name:'ImageDirector7',x:495,y:0,hitArray:[]});
